cf104cf700e96bcb1cff574e6903f18a6e9902a5
Jelmer Cormont <jcormont@gmail.com>
2017-08-01 22:10:17 +0800

    Export Promise polyfill in DOM index

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -8,6 +8,6 @@ import * as core from "@typescene/core";
 // export all modules as global variables
 var w: any = window;
 w["typescene"] = { Async, UI, App, core };
-w["Async"] = Async;
-w["UI"] = UI;
-w["App"] = App;
+
+// export Promise polyfill if needed
+if (!w["Promise"]) w["Promise"] = Async.Promise;
