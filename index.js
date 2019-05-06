45e0ac9922a7a0e2036cff179cf03626b1d5b1b8
Jelmer Cormont <jcormont@gmail.com>
2019-05-06 20:57:08 +0800

    Add comment header to index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+/*! *******************************************************
+Typescene framework -- https://typescene.dev
+See LICENSE and README files for usage details
+******************************************************** */
 export * from "./dist";
 
