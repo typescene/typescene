a6cb38ff86244ef5cbedf2c677c93d51ad608630
Jelmer Cormont <jcormont@gmail.com>
2019-08-10 10:06:45 +0800

    Fix private properties with underscores that are used by name (#25)

--- src/app/AppActivityList.ts
+++ src/app/AppActivityList.ts
@@ -17,12 +17,12 @@ export class AppActivityList extends Component {
     /** Create an empty list */
     constructor() {
         super();
-        this._list = new ManagedList();
-        this._list.restrict(AppActivity);
+        this.$list = new ManagedList();
+        this.$list.restrict(AppActivity);
 
         // propagate list events, active/inactive
         this.propagateChildEvents();
-        this._list.propagateEvents(e => {
+        this.$list.propagateEvents(e => {
             if (e === ACTIVE || e === INACTIVE) {
                 return CHANGE;
             }
@@ -31,50 +31,50 @@ export class AppActivityList extends Component {
 
     /** Add one or more child activities to the list (see `ManagedList.add`) */
     add(...activities: AppActivity[]) {
-        this._list.add(...activities);
+        this.$list.add(...activities);
         return this;
     }
 
     /** Clear the list (see `ManagedList.clear`) */
     clear() {
-        this._list.clear();
+        this.$list.clear();
         return this;
     }
 
     /** The number of activities currently in the list (see `ManagedList.count`) */
-    get count() { return this._list.count }
+    get count() { return this.$list.count }
 
     /** Returns the activity with given ID (see `ManagedObject.managedId`). */
-    find(id: number) { return this._list.find(id) }
+    find(id: number) { return this.$list.find(id) }
 
     /** Returns the first activity in the list (see `ManagedList.first`) */
-    first() { return this._list.first() }
+    first() { return this.$list.first() }
 
     /** Returns the activity at given position in the list (0 based; see `ManagedList.get`) */
-    get(index: number) { return this._list.get(index) }
+    get(index: number) { return this.$list.get(index) }
 
     /** Returns true if given activity is currently included in this list (see `ManagedList.includes`) */
-    includes(activity: AppActivity) { return this._list.includes(activity) }
+    includes(activity: AppActivity) { return this.$list.includes(activity) }
 
     /** Returns the last activity in the list (see `ManagedList.last`) */
-    last() { return this._list.last() }
+    last() { return this.$list.last() }
 
     /** Remove given activity from the list. Does not throw an error if the activity was not included in the list (see `ManagedList.remove`) */
     remove(activity: AppActivity) {
-        this._list.remove(activity);
+        this.$list.remove(activity);
         return this;
     }
 
     /** Replace the activities in the list with given activities (see `ManagedList.replace`) */
     replace(activities: Iterable<AppActivity>) {
-        this._list.replace(activities);
+        this.$list.replace(activities);
         return this;
     }
 
     /** Returns an array with all activities currently in this list (see `ManagedList.toArray`) */
-    toArray() { return this._list.toArray() }
+    toArray() { return this.$list.toArray() }
 
     /** The encapsulated list itself */
     @managedChild
-    private _list!: ManagedList<AppActivity>;
+    private $list!: ManagedList<AppActivity>;
 }
