d79e21adbc35c77b958cda047f24bb04bc190442
Jelmer Cormont <jcormont@gmail.com>
2021-05-05 11:05:02 +0800

    Add viewport context to support responsive design

--- src/app/AppComponent.ts
+++ src/app/AppComponent.ts
@@ -1,5 +1,5 @@
 import { bind, Component, managed } from "../core";
-import { renderContextBinding, UIRenderContext } from "../ui";
+import { renderContextBinding, UIRenderContext, viewportContextBinding } from "../ui";
 import { AppActivationContext } from "./AppActivationContext";
 
 /** @internal Activation context binding, can be reused to avoid creating new bindings */
@@ -8,11 +8,16 @@ export const activationContextBinding = bind("activationContext");
 /** Specialized `Component` that propagates application properties (abstract) */
 export abstract class AppComponent extends Component.with({
   renderContext: renderContextBinding,
+  viewportContext: viewportContextBinding,
   activationContext: activationContextBinding,
 }) {
   /** Application render context, propagated from the parent composite object */
   @managed
   renderContext?: UIRenderContext;
+
+  /** Observable viewport context data, propagated from the parent composite object */
+  @managed
+  viewportContext?: any;
 
   /** Activation context, propagated from the parent composite object */
   @managed
